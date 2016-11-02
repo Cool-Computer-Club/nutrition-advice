182f90a9cdc04dfd13cd4cf3de08bca1da97b807
John Whiles <johnwhiles@outlook.com>
2016-11-02 13:56:36 +0000

    introduce nutrition api js file

--- src/nutrition.js
+++ src/nutrition.js
@@ -1,16 +1,7 @@
-var example = 'https://api.nutritionix.com/v1_1/search/apple?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories&appId=97d9d185&appKey=0eb85f7f785232907b4390b5636f1610'
-
-
 var firstPart = 'https://api.nutritionix.com/v1_1/search/'
 var secondPart = '?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Citem_type=3&app'
-
 var searchTerm = 'aubergine'
-
-
 var apiAddress = `${firstPart}${searchTerm}${secondPart}Id=${apiID}&appKey=${apiKey}`
-
-console.log(apiAddress === example);
-
 var xhr = new XMLHttpRequest();
 
 xhr.addEventListener('load', function () {
