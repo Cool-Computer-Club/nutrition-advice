3ceb3b71c736e3d53953c662e068e5ab3c01b468
John Whiles <johnwhiles@outlook.com>
2016-11-02 16:55:17 +0000

    connect search field and button to our nutrition api

--- src/nutrition.js
+++ src/nutrition.js
@@ -5,15 +5,22 @@ var searchTerm = newSearchTerm
 var apiAddress = `${firstPart}${searchTerm}${secondPart}appId=${apiID}&appKey=${apiKey}`
 var xhr = new XMLHttpRequest();
 
-  function appendNutrition (tagType, name, tagContent = '') {
+  function appendNutrition (tagType, name, tagContent = '', targetID = 'ingredient') {
     var newtritionElement = document.createElement(tagType);
     var newtritionContent = document.createTextNode(name + ' ' + tagContent);
     newtritionElement.append(newtritionContent);
-    document.getElementById('ingredient').append(newtritionElement);
+    document.getElementById(targetID).append(newtritionElement);
   }
 
 xhr.addEventListener('load', function () {
+  document.getElementById('ingredient').innerHTML = '';
+  document.getElementById('error').innerHTML = '';
+  document.getElementById('input').value = '';
   var result = JSON.parse(xhr.responseText);
+  if (result.total_hits === 0) {
+    appendNutrition('h3', searchTerm + ' aint a thing', '', 'error')
+    return;
+  }
   var itemName = result.hits[0].fields.item_name;
   var calories = result.hits[0].fields.nf_calories;
   var sugars = result.hits[0].fields.nf_sugars;
@@ -28,3 +35,9 @@ xhr.addEventListener('load', function () {
 xhr.open('GET', apiAddress);
 xhr.send();
 };
+
+
+document.getElementById('search').addEventListener('click', function() {
+  var inputField = document.getElementById('input').value;
+  nutrition(inputField);
+})
