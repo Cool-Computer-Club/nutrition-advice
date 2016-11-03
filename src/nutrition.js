0d6f14400a39c608a1ab4a2c5aac7c125ee326b0
jsms90 <jsms@hotmail.co.uk>
2016-11-03 18:25:41 +0000

    separate functions inside foodFinder

--- src/nutrition.js
+++ src/nutrition.js
@@ -5,19 +5,18 @@ var foodFinder = (function () {
     return active;
   }
 
-  self.nutrition = function (newSearchTerm) {
-    var firstPart = 'https://api.nutritionix.com/v1_1/search/'
-    var secondPart = '?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_sugars%2Cnf_calories%2Cnf_dietary_fiber&'
-    var searchTerm = newSearchTerm
-    var apiAddress = `${firstPart}${searchTerm}${secondPart}appId=${nutritionApiID}&appKey=${nutritionApiKey}`
-    var xhr = new XMLHttpRequest();
+  self.appendNutrition = function (tagType, name, tagContent = '', targetID = 'ingredient') {
+    var newtritionElement = document.createElement(tagType);
+    var newtritionContent = document.createTextNode(name + ' ' + tagContent);
+    newtritionElement.appendChild(newtritionContent);
+    document.getElementById(targetID).appendChild(newtritionElement);
+  }
 
-    function appendNutrition (tagType, name, tagContent = '', targetID = 'ingredient') {
-      var newtritionElement = document.createElement(tagType);
-      var newtritionContent = document.createTextNode(name + ' ' + tagContent);
-      newtritionElement.appendChild(newtritionContent);
-      document.getElementById(targetID).appendChild(newtritionElement);
-    }
+  self.nutrition = function (searchTerm) {
+    var urlBase = 'https://api.nutritionix.com/v1_1/search/'
+    var urlEnd = '?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_sugars%2Cnf_calories%2Cnf_dietary_fiber&'
+    var apiAddress = `${urlBase}${searchTerm}${urlEnd}appId=${nutritionApiID}&appKey=${nutritionApiKey}`
+    var xhr = new XMLHttpRequest();
 
     xhr.addEventListener('load', function () {
       var result = JSON.parse(xhr.responseText);
@@ -26,16 +25,16 @@ var foodFinder = (function () {
       var sugars = result.hits[0].fields.nf_sugars;
       var fiber = result.hits[0].fields.nf_dietary_fiber;
       if (result.total_hits === 0) {
-        appendNutrition('p', searchTerm + " isn't really something you should eat...", '', 'error');
+        foodFinder.appendNutrition('p', searchTerm + " isn't really something you should eat...", '', 'error');
         document.getElementById('results').style.visibility = 'hidden';
         document.getElementById('about').style.visibility = 'hidden';
         return;
       }
-      appendNutrition('h3', itemName);
-      appendNutrition('p', 'Calories: ', calories);
-      appendNutrition('p', 'Sugars: ', sugars);
-      appendNutrition('p', 'Fiber: ', fiber);
-      document.getElementById('ingredient').innerHTML = '';
+      foodFinder.appendNutrition('h3', itemName);
+      foodFinder.appendNutrition('p', 'Calories: ', calories);
+      foodFinder.appendNutrition('p', 'Sugars: ', sugars);
+      foodFinder.appendNutrition('p', 'Fiber: ', fiber);
+      // document.getElementById('ingredient').innerHTML = '';
       document.getElementById('error').innerHTML = '';
       document.getElementById('results').style.visibility = 'visible';
       document.getElementById('about').style.visibility = 'hidden';
@@ -55,6 +54,7 @@ var foodFinder = (function () {
       return;
     }
     var inputField = document.getElementById('input').value;
+    console.log('yo')
     spellcheck(inputField);
   })
 
