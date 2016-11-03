f9c82bd34db32de82ac25789bbeb1b55cabdf9eb
jsms90 <jsms@hotmail.co.uk>
2016-11-03 15:43:11 +0000

    test whether about text returns when user clicks search on an empty field

--- specs/tests.js
+++ specs/tests.js
@@ -1,7 +1,5 @@
 QUnit.test('clicking on search activates function', function( assert ) {
-  console.log(active);
   document.getElementById('search').click();
-  console.log("after click: ",active);
   assert.equal(active, true, 'Passed');
 });
 
@@ -9,4 +7,17 @@ QUnit.test('if user searches nothing, error is shown', function( assert ) {
   document.getElementById('input').value = '';
   document.getElementById('search').click();
   assert.equal(document.getElementById('error').innerHTML, 'We need an input! Ya silly', 'Passed');
+});
+
+QUnit.test('results of nutrition API call are hidden if user puts nothing in search field', function( assert ) {
+  document.getElementById('input').value = '';
+  document.getElementById('search').click();
+  assert.equal(document.getElementById('results').style.visibility, 'hidden', 'Passed');
+});
+
+QUnit.test('about text becomes visible again if user searches something, then clicks search with an empty field', function( assert ) {
+  document.getElementById('about').style.visibility = 'hidden';
+  document.getElementById('input').value = '';
+  document.getElementById('search').click();
+  assert.equal(document.getElementById('about').style.visibility, 'visible', 'Passed');
 });
