1ca351202c3ec58f9b25a79b8621c8999b6f5778
jsms90 <jsms@hotmail.co.uk>
2016-11-03 15:27:40 +0000

    add test for empty search throwing error on DOM

--- specs/tests.js
+++ specs/tests.js
@@ -4,3 +4,9 @@ QUnit.test('clicking on search activates function', function( assert ) {
   console.log("after click: ",active);
   assert.equal(active, true, 'Passed');
 });
+
+QUnit.test('if user searches nothing, error is shown', function( assert ) {
+  document.getElementById('input').value = '';
+  document.getElementById('search').click();
+  assert.equal(document.getElementById('error').innerHTML, 'We need an input! Ya silly', 'Passed');
+});
