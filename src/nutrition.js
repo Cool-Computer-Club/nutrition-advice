1f620d83e23fbd5114cbfcdcb77f68a0b4a4f3f4
John Whiles <johnwhiles@outlook.com>
2016-11-03 10:55:33 +0000

    make elements hide/unhide on search

--- src/nutrition.js
+++ src/nutrition.js
@@ -2,7 +2,7 @@ function nutrition(newSearchTerm) {
 var firstPart = 'https://api.nutritionix.com/v1_1/search/'
 var secondPart = '?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_sugars%2Cnf_calories%2Cnf_dietary_fiber&'
 var searchTerm = newSearchTerm
-var apiAddress = `${firstPart}${searchTerm}${secondPart}appId=${apiID}&appKey=${apiKey}`
+var apiAddress = `${firstPart}${searchTerm}${secondPart}appId=${nutritionApiID}&appKey=${nutritionApiKey}`
 var xhr = new XMLHttpRequest();
 
   function appendNutrition (tagType, name, tagContent = '', targetID = 'ingredient') {
@@ -18,7 +18,9 @@ xhr.addEventListener('load', function () {
   document.getElementById('input').value = '';
   var result = JSON.parse(xhr.responseText);
   if (result.total_hits === 0) {
-    appendNutrition('p', searchTerm + " isn't really something you should eat...", '', 'error')
+    appendNutrition('p', searchTerm + " isn't really something you should eat...", '', 'error');
+    document.getElementById('results').style.visibility = 'hidden';
+    document.getElementById('about').style.visibility = 'visible';
     return;
   }
   var itemName = result.hits[0].fields.item_name;
@@ -30,6 +32,8 @@ xhr.addEventListener('load', function () {
   appendNutrition('p', 'Sugars: ', sugars);
   appendNutrition('p', 'Fiber: ', fiber);
   console.log(itemName);
+  document.getElementById('results').style.visibility = 'visible';
+  document.getElementById('about').style.visibility = 'hidden';
 });
 
 xhr.open('GET', apiAddress);
@@ -41,6 +45,8 @@ document.getElementById('search').addEventListener('click', function() {
   if (!document.getElementById('input').value) {
     document.getElementById('ingredient').innerHTML = '';
     document.getElementById('error').innerHTML = 'We need an input! Ya silly';
+    document.getElementById('results').style.visibility = 'hidden';
+    document.getElementById('about').style.visibility = 'visible';
     return;
   }
   var inputField = document.getElementById('input').value;
