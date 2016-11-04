c02a741334caa66f46ebf8d5e6ab81ee3e82678f
Esraa <jbesraa@gmail.com>
2016-11-04 13:12:43 +0000

    make specrunner as index.html

--- src/vision.js
+++ src/vision.js
@@ -1,17 +1,9 @@
 var describeImage = new XMLHttpRequest();
 
 describeImage.addEventListener('load', function() {
-<<<<<<< HEAD
     var result = JSON.parse(describeImage.responseText)
     foodFinder.spellcheck(result.tags[0].name);
   })
-=======
-  var result = JSON.parse(describeImage.responseText);
-  console.log(result);
-  foodFinder.spellcheck(result.tags[0].name);
-})
->>>>>>> a805920252a2f6566f4abf673006bb195eb9b076
-
 var getImageDescription = function (url) {
   describeImage.open('POST', 'https://api.projectoxford.ai/vision/v1.0/tag?maxCandidates=1');
   describeImage.setRequestHeader('Content-Type', 'application/json');
